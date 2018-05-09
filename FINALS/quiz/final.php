178e0afa0f95ac16b68b21194057fe30fda6514c
marinelleAlegre <biku.tomoshimasen@gmail.com>
2018-05-09 23:55:19 +0800

    fixed placing of the files

---
+++ FINALS/quiz/final.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>PHP Quiz</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>WEBTEK QUIZ</h1>
+        </div>
+    </header>
+    
+    <main>
+        <div class="container">
+            <h2>You're Done!</h2>
+            <p>Congrats! You have completed the test</p>
+            <p>Final Score: 5</p>
+            <a href="question.php?n=1" class="start" class="start">Take Again</a>
+        </div>
+    </main>
+    
+    <footer>
+        <div class="container">
+            Copyright &copy;2018, Web Systems and Technology (Group 2)
+        </div>
+    </footer>
+</body>
+</html>
---
+++ FINALS/quiz/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>PHP Quiz</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>WEBTEK QUIZ</h1>
+        </div>
+    </header>
+    
+    <main>
+        <div class="container">
+            <h2> QUIZ ON JAVASCRIPT</h2>
+            <p>This is a multiple choice quiz to test your knowledge on Javascript</p>
+            
+            <ul>
+                <li><strong>Number of question</strong>5</li>
+                <li><strong>Type: </strong> Multiple Choice</li>
+                <li><strong>Estimated Time:</strong>4 minutes</li>
+            </ul>
+            <a href="question.php?n=1" class="start">Start Quiz!</a>
+        </div>
+    </main>
+    
+    <footer>
+        <div class="container">
+            Copyright &copy;2018, Web Systems and Technology (Group 2)
+        </div>
+    </footer>
+</body>
+</html>
