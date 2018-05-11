0c5d7e44af05928cd5f3090670038c0234e288ba
marinelleAlegre <biku.tomoshimasen@gmail.com>
2018-05-11 22:48:29 +0800

    Edited minor changes and deleted html files

---
+++ FINALS/index.php
@@ -0,0 +1,378 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>WEB SYSTEMS AND TECHNOLOGIES</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="img/logo.png">
+        
+       
+        <!-- Google Fonts -->
+        <link href='https://fonts.googleapis.com/css?family=Lato:400,700,400italic,300' rel='stylesheet' type='text/css'>
+
+		<!-- all css here -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/animated.css">
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/owl.transitions.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/animate.css">
+		<link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+     
+        
+        <header>
+            <!-- Start Top Header -->
+            <div id="sticker" class="header-area">
+                <div class="container">
+                    <nav class="navbar navbar-default">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        <a class="navbar-brand page-scroll" href="#page-top"><img src="img/logo/logo.png" alt=""></a>
+                        </div>
+                        <div class="collapse navbar-collapse main-menu" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="active">
+                                    <a class="page-scroll" href="#home">Home</a>
+                                </li>
+                                <li>
+                                    <a class="page-scroll" href="#about">Notes</a>
+                                </li>
+                                <li>
+                                    <a class="page-scroll" href="#reviews">Team</a>
+                                </li>		              	
+                                <li>
+                                    <a class="page-scroll" href="#blog">References</a>
+                                </li>		              	              	              	
+                                <li>
+                                    <a class="page-scroll" href="../FINALS/login/login1.php">Login</a>
+                                </li>		              			              
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div id="home" class="header-img">
+                <div class="header-overlay"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-left">
+                            <div class="header-bottom">
+                                <h1>Web System and Technologies </h1>
+                                <h2 class="cd-headline clip is-full-width">
+                                    <span class="cd-words-wrapper">
+                                        <b class="is-visible">School of Computing Information Sciences</b>
+                                        <b>Final Course Website</b>
+                                        <b>Web Development with no limits</b>
+                                    </span>
+                                </h2>
+                                <a href="#" class="btn custom-btn">LECTURE</a><span class="btn-2"><a href="#" class="btn custom-btn">FINALS</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+		<div id ="about" class="about-area page-scroll area-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-sm-12">
+						<div class="section-head">
+						    <h3>NOTES: </h3>
+						</div>
+					</div>
+				</div>
+				<div class="row second-row">
+				    <div class="col-md-3 col-sm-4">
+				        <div class="about-details text-center">
+				            <div class="single-about">
+				                <a class="about-icon" href="#"><i class="fa fa-tablet"></i></a>
+				                <h4>Java Web Servlets</h4>
+                                <p>SHORT DESCRIPTION DITO</p>
+				            </div>
+				        </div>
+				    </div>
+				    <div class="col-md-3 col-sm-4">
+				        <div class="about-details text-center">
+				            <div class="single-about">
+				                <a class="about-icon" href="#"><i class="fa fa-code"></i></a>
+				                <h4>Server-Side JavaScript</h4>
+                                <p>SHORT DESCRIPTION DITO</p>
+				            </div>
+				        </div>
+				    </div>
+				    <div class="col-md-3 col-sm-4">
+				        <div class="about-details text-center">
+				            <div class="single-about">
+				                <a class="about-icon" href="#"><i class="fa fa-laptop"></i></a>
+				                <h4>PHP</h4>
+                                <p>SHORT DESCRIPTION DITO</p>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+			</div>
+		</div>
+
+		<div id="reviews" class="Reviews-area">
+            <div class="head-overly"></div>
+		    <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
+                        <div class="Reviews-content">
+                            <div class="testimonial-carousel item-indicator">
+                                <div class="single-testi text-center">
+                                    <div class="testi-img">
+                                        <img src="img/testimonial/test1.jpg" alt="">
+                                    </div>
+                                    <div class="testi-text text-center">
+                                        <h4>Marinelle Alegre</h4>
+                                        <span class="guest-rev">2152394@slu.edu.ph</span>
+                                    </div>
+                                </div>
+
+                                <div class="single-testi text-center">
+                                    <div class="testi-img">
+                                        <img src="img/testimonial/test2.jpg" alt="">
+                                    </div>
+                                    <div class="testi-text text-center">
+                                        <h4>John Martin Baladad</h4>
+                                        <span class="guest-rev">2153882@slu.edu.ph</a></span>
+                                    </div>
+                                </div>
+                                <!-- End single item -->
+                                <div class="single-testi text-center">
+                                    <div class="testi-img">
+                                        <img src="img/testimonial/test3.jpg" alt="">
+                                    </div>
+                                    <div class="testi-text text-center">
+                                        <h4>Veronica Leonardo</h4>
+                                        <span class="guest-rev">2152028@slu.edu.ph</a></span>
+                                    </div>
+                                </div>
+                                <!-- End single item -->
+								<div class="single-testi text-center">
+                                    <div class="testi-img">
+                                        <img src="img/testimonial/test4.jpg" alt="">
+                                    </div>
+                                    <div class="testi-text text-center">
+                                        <h4>Denyse Rae Cayadi</h4>
+                                        <span class="guest-rev">2146790@slu.edu.ph</a></span>
+                                    </div>
+                                </div>
+								<div class="single-testi text-center">
+                                    <div class="testi-img">
+                                        <img src="img/testimonial/test5.jpg" alt="">
+                                    </div>
+                                    <div class="testi-text text-center">
+                                        <h4>Steven Mark Gerald Mangati</h4>
+                                        <span class="guest-rev">2166769@slu.edu.ph</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+		    </div>
+		</div>
+
+		<div id="blog" class="blog-area area-padding">
+		    <div class="container">
+		        <div class="row">
+		            <div class="col-md-12">
+                        <div class="section-head">
+                            <h3>Latest blog.</h3>
+                        </div>
+                    </div>
+		        </div>
+		        <div class="row">
+        			<div class="blog-item">
+        				<div class="col-md-4 col-sm-4 col-xs-12">
+        					<div class="single-blog">
+        						<div class="single-blog-img">
+        							<a href="#">
+        								<img src="img/blog/blog1.jpg" alt="">
+        							    <span class="overly"></span>
+        							</a>
+        						</div>
+        						<div class="blog-content">
+									<h4><a href="#">Post my imagine Items</a></h4>
+        							<p>Lorem ipsum dolor sit amet conse adipis elit Assumenda repud eum veniam optio modi sit explicabo nisi magnam quibusdam.</p>
+        						</div>
+        						<div class="blog-meta">
+        							<span class="date-type">
+        								<i class="fa fa-calendar"></i>2016-02-05 / 07:10:45
+        							</span>
+        							<span class="comments-type">
+										<i class="fa fa-comment-o"></i>
+										<a href="#">80 comments</a>
+									</span>
+        						</div>
+        					</div>
+        				</div>
+        				<!-- single blog -->
+        				<div class="col-md-4 col-sm-4 col-xs-12">
+        					<div class="single-blog">
+        						<div class="single-blog-img">
+        							<a href="#">
+        								<img src="img/blog/blog2.jpg" alt="">
+        							    <span class="overly"></span>
+        							</a>
+        						</div>
+        						<div class="blog-content">
+									<h4><a href="#">Discovering Client Stories</a></h4>
+        							<p>Lorem ipsum dolor sit amet conse adipis elit Assumenda repud eum veniam optio modi sit explicabo nisi magnam quibusdam.</p>
+        						</div>
+        						<div class="blog-meta">
+        							<span class="date-type">
+        								<i class="fa fa-calendar"></i>2016-03-05 / 09:10:16
+        							</span>
+        							<span class="comments-type">
+										<i class="fa fa-comment-o"></i>
+										<a href="#">130 comments</a>
+									</span>
+        						</div>
+        					</div>
+        				</div>
+        				<!-- single blog -->
+        				<div class="col-md-4 col-sm-4 col-xs-12">
+        					<div class="single-blog">
+        						<div class="single-blog-img">
+        							<a href="#">
+        								<img src="img/blog/blog3.jpg" alt="">
+        							    <span class="overly"></span>
+        							</a>
+        						</div>
+        						<div class="blog-content">
+									<h4><a href="#">The mystery of digital experience</a></h4>
+        							<p>Lorem ipsum dolor sit amet conse adipis elit Assumenda repud eum veniam optio modi sit explicabo nisi magnam quibusdam.</p>
+        						</div>
+        						<div class="blog-meta">
+        							<span class="date-type">
+        								<i class="fa fa-calendar"></i>2016-04-07 / 08:10:18
+        							</span>
+        							<span class="comments-type">
+										<i class="fa fa-comment-o"></i>
+										<a href="#">30 comments</a>
+									</span>
+        						</div>
+        					</div>
+        				</div>
+        				<!-- single blog -->
+        				<div class="col-md-4 col-sm-4 col-xs-12">
+        					<div class="single-blog">
+        						<div class="single-blog-img">
+        							<a href="#">
+        								<img src="img/blog/blog4.jpg" alt="">
+        							    <span class="overly"></span>
+        							</a>
+        						</div>
+        						<div class="blog-content">
+									<h4><a href="#">Startup – Always a New Beginning</a></h4>
+        							<p>Lorem ipsum dolor sit amet conse adipis elit Assumenda repud eum veniam optio modi sit explicabo nisi magnam quibusdam.</p>
+        						</div>
+        						<div class="blog-meta">
+        							<span class="date-type">
+        								<i class="fa fa-calendar"></i>2016-04-07 / 08:10:18
+        							</span>
+        							<span class="comments-type">
+										<i class="fa fa-comment-o"></i>
+										<a href="#">30 comments</a>
+									</span>
+        						</div>
+        					</div>
+        				</div>
+        				
+        				<div class="col-md-4 col-sm-4 col-xs-12">
+        					<div class="single-blog">
+        						<div class="single-blog-img">
+        							<a href="#">
+        								<img src="img/blog/blog5.jpg" alt="">
+        							    <span class="overly"></span>
+        							</a>
+        						</div>
+        						<div class="blog-content">
+									<h4><a href="#">Crafting unique designs</a></h4>
+        							<p> versions have evolved over the years, sometimes by accident, Various versions have evolved over the years, sometimes  have evolved over the years,</p>
+        						</div>
+        						<div class="blog-meta">
+        							<span class="date-type">
+        								<i class="fa fa-calendar"></i>2016-04-07 / 08:10:18
+        							</span>
+        							<span class="comments-type">
+										<i class="fa fa-comment-o"></i>
+										<a href="#">30 comments</a>
+									</span>
+        						</div>
+        					</div>
+        				</div>
+        				<!-- single blog -->
+        				<div class="col-md-4 col-sm-4 col-xs-12">
+        					<div class="single-blog">
+        						<div class="single-blog-img">
+        							<a href="#">
+        								<img src="img/blog/blog6.jpg" alt="">
+        							    <span class="overly"></span>
+        							</a>
+        						</div>
+        						<div class="blog-content">
+									<h4><a href="#">Let start your creative design with Adobe</a></h4>
+        							<p> versions have evolved over the years, sometimes by accident, Various versions have evolved over the years, sometimes  have evolved over the years,</p>
+        						</div>
+        						<div class="blog-meta">
+        							<span class="date-type">
+        								<i class="fa fa-calendar"></i>2016-04-07 / 08:10:18
+        							</span>
+        							<span class="comments-type">
+										<i class="fa fa-comment-o"></i>
+										<a href="#">30 comments</a>
+									</span>
+        						</div>
+        					</div>
+        				</div>
+        		
+        			</div>
+        		</div>
+		    </div>
+		</div>
+	
+            <div class="footer-area">
+               <div class="container">
+                   <div class="row">
+                      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                          <div class="copyright text-center">
+                            <p>Copyright © 2018 <a> Web System and Technologies Course Website</a>.All Rights Reserved</p>
+                          </div>
+                       </div>
+                   </div>
+               </div>
+            </div> 
+            
+		<!-- all js here -->
+        <script src="js/vendor/jquery-1.12.4.min.js"></script>
+		<!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/easing.js"></script>
+        <script src="js/jquery.appear.js"></script>
+        <script src="js/animated.js"></script>
+        <script src="js/jquery.mixitup.min.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/jquery.counterup.min.js"></script>
+        <script src="js/waypoints.js"></script>
+        <script src="js/plugins.js"></script>
+		<!-- main js -->
+        <script src="js/main.js"></script>
+    </body>	
+	
+</html>			
---
+++ FINALS/userIndex.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>WEB SYSTEMS AND TECHNOLOGIES</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="img/logo.png">
+        
+       
+        <!-- Google Fonts -->
+        <link href='https://fonts.googleapis.com/css?family=Lato:400,700,400italic,300' rel='stylesheet' type='text/css'>
+
+		<!-- all css here -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/animated.css">
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/owl.transitions.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/animate.css">
+		<link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+     
+        
+        <header>
+            <!-- Start Top Header -->
+            <div id="sticker" class="header-area">
+                <div class="container">
+                    <nav class="navbar navbar-default">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        <a class="navbar-brand page-scroll" href="#page-top"><img src="img/logo/logo.png" alt=""></a>
+                        </div>
+                        <div class="collapse navbar-collapse main-menu" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="active">
+                                    <a class="page-scroll" href="#home">Home</a>
+                                </li>
+                                <li>
+                                    <a class="page-scroll" href="#">Java Web Servlets</a>
+                                </li>
+                                <li>
+                                    <a class="page-scroll" href="#">Node.JS</a>
+                                </li>		              	
+                                <li>
+                                    <a class="page-scroll" href="#">PHP</a>
+                                </li>			              	
+                                <li>
+                                    <a class="page-scroll" href="#">Quiz</a>
+                                </li>		              	              	              	
+                                <li>
+                                    <a class="page-scroll" href="../FINALS/logout.php">Logout</a>
+                                </li>		              			              
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div id="home" class="header-img">
+                <div class="header-overlay"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-left">
+                            <div class="header-bottom">
+                                <h1>Web System and Technologies </h1>
+                                <h2 class="cd-headline clip is-full-width">
+                                    <span class="cd-words-wrapper">
+                                        <b class="is-visible">School of Computing Information Sciences</b>
+                                        <b>Final Course Website</b>
+                                        <b>Web Development with no limits</b>
+                                    </span>
+                                </h2>
+                                <!--a href="#" class="btn custom-btn">LECTURE</a><span class="btn-2"><a href="#" class="btn custom-btn">FINALS</a></span-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+		<div id ="about" class="about-area page-scroll area-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-sm-12">
+						<div class="section-head">
+						    <h3>NOTES: </h3>
+						</div>
+					</div>
+				</div>
+				<div class="row second-row">
+				    <div class="col-md-3 col-sm-4">
+				        <div class="about-details text-center">
+				            <div class="single-about">
+				                <a class="about-icon" href="#"><i class="fa fa-tablet"></i></a>
+				                <h4>Java Web Servlets</h4>
+				                <p>- A servlet is a Java programming language class used to extend the capabilities of servers that host applications accessed by means of a request-response programming model. Although servlets can respond to any type of request, they are commonly used to extend the applications hosted by web servers. For such applications, Java Servlet technology defines HTTP-specific servlet classes</p>
+								<p>
+				            </div>
+				        </div>
+				    </div>
+				    <div class="col-md-3 col-sm-4">
+				        <div class="about-details text-center">
+				            <div class="single-about">
+				                <a class="about-icon" href="#"><i class="fa fa-code"></i></a>
+				                <h4>Server-Side JavaScript</h4>
+				                <p>- Java Server Pages (JSP) is a server-side programming technology that enables the creation of dynamic, platform-independent method for building Web-based applications. JSP have access to the entire family of Java APIs, including the JDBC API to access enterprise databases.
+								<p>
+								</p>
+				            </div>
+				        </div>
+				    </div>
+				    <div class="col-md-3 col-sm-4">
+				        <div class="about-details text-center">
+				            <div class="single-about">
+				                <a class="about-icon" href="#"><i class="fa fa-laptop"></i></a>
+				                <h4>PHP</h4>
+				                <p>- PHP (recursive acronym for PHP: Hypertext Preprocessor) is a widely-used open source general-purpose scripting language that is especially suited for web development and can be embedded into HTML</p>
+				            </div>
+				        </div>
+				    </div>
+				    <div class="col-md-3 hidden-sm">
+				        <div class="about-details text-center">
+				            <div class="single-about">
+				                <a class="about-icon" href="#"><i class="fa fa-desktop"></i></a>
+				                <h4>Web Application Security</h4>
+				                <p>- Web application security is a branch of Information Security that deals specifically with security of websites, web applications and web services. At a high level, Web application security draws on the principles of application security but applies them specifically to Internet and Web systems</p>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+				<div class="row second-row">
+				    <div class="col-md-3 col-sm-4">
+				        <div class="about-details text-center">
+				            <div class="single-about">
+				                <a class="about-icon" href="#"><i class="fa fa-tablet"></i></a>
+				                <h4>NodesJs</h4>
+				                <p>- Node.js is a runtime environment for building fast and scalable network applications easily with JavaScript. Node.js uses an event driven, non blocking I/O model that makes it lightweight and efficient, perfect for data intensive real time applications that run across distributed devices. It  provides a rich library of various JavaScript modules which simplifies the development of web applications using Node.js to a great extent.</p>
+				            </div>
+				        </div>
+				    </div>
+				<div class="row fix">
+				    <div class="col-md-6 col-sm-6">
+				        <div class="self-img">
+				            <a href="#"><img src="img/background/bg4.jpg" alt=""></a>
+				        </div>
+				    </div>
+				    <div class="col-md-6 col-sm-6">
+				        <div class="about-self">
+				            <div class="self-head">
+				                <h4 class="intro-head">Introduction</h4>
+				                <p>INSERT</p>
+				            </div>
+				            <div class="self-text">
+				                <ul>
+				                    <li>insert</li>
+				                    <li>insert</li>
+				                    <li>insert</li>
+				                    <li>insert</li>
+                                    <li>insert</li>
+                                    <li>insert</li>
+				                </ul>
+				            </div>
+				            
+				        </div>
+				    </div>
+				</div>
+			</div>
+		</div>
+
+            <div class="footer-area">
+               <div class="container">
+                   <div class="row">
+                      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                          <div class="copyright text-center">
+                            <p>Copyright © 2018 <a> Web System and Technologies Course Website</a>.All Rights Reserved</p>
+                          </div>
+                       </div>
+                   </div>
+               </div>
+            </div> 
+            
+		<!-- all js here -->
+        <script src="js/vendor/jquery-1.12.4.min.js"></script>
+		<!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/easing.js"></script>
+        <script src="js/jquery.appear.js"></script>
+        <script src="js/animated.js"></script>
+        <script src="js/jquery.mixitup.min.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/jquery.counterup.min.js"></script>
+        <script src="js/waypoints.js"></script>
+        <script src="js/plugins.js"></script>
+		<!-- main js -->
+        <script src="js/main.js"></script>
+    </body>	
+	
+</html>			
